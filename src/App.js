e929497418d7bb368ca196cd75933487185985c5
ehsan2793 <ehsan2793@gmail.com>
2022-02-16 04:11:01 -0800

    card added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ExpenseItem from './components/ExpenseItem';
 import expenses from './data/expenses';
+import Card from './components/Card'
 import './App.css';
 const App = () => {
   return (
     <div>
       <h1>hello world</h1>
-      <div className="expenses">
+      <Card className="expenses">
         {expenses.map((item) => (
           <ExpenseItem key={Math.random(Math.random()) * 1000} items={item} />
         ))}
-      </div>
+      </Card>
     </div>
   );
 };
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 const ExpenseItem = (props) => {
     const { date, title, amount } = props.items;
 
     return (
-        <div className="expense-item ">
+        <Card className="expense-item ">
             <ExpenseDate date={date} />
             <div className="expense-item_description">
                 <h2>{title}</h2>
                 <div className="expense-item_price">{amount}</div>
             </div>
-        </div>
+        </Card>
     );
 };
 
