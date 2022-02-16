b0a8ef69268361e88cad5239608859b660f732f2
ehsan2793 <ehsan2793@gmail.com>
2022-02-16 03:30:54 -0800

    app.css added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ExpenseItem from './components/ExpenseItem';
 import expenses from './data/expenses';
+import './App.css';
 const App = () => {
   return (
     <div>
       <h1>hello world</h1>
-      {expenses.map((item) => (
-        <ExpenseItem key={Math.random(Math.random()) * 1000} items={item} />
-      ))}
+      <div className="expenses">
+        {expenses.map((item) => (
+          <ExpenseItem key={Math.random(Math.random()) * 1000} items={item} />
+        ))}
+      </div>
     </div>
   );
 };
