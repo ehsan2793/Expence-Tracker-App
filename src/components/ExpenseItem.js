08f332b5e670c4681745c17260b618fcaa86b148
ehsan2793 <ehsan2793@gmail.com>
2022-02-16 02:26:57 -0800

    data added correctly

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,16 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
     const { date, title, amount } = props.items;
+    const month = date.toLocaleDateString('en-US', { month: "long" })
+    const day = date.toLocaleDateString('en-US', { day: "2-digit" })
+    const year = date.getFullYear()
     return (
         <div className="expense-item ">
-            <div>{date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item_description">
                 <h2>{title}</h2>
                 <div className="expense-item_price">{amount}</div>
--- src/data/expenses.js
+++ src/data/expenses.js
@@ -2,27 +2,27 @@ const expenses = [
     {
         title: 'Car Insurance',
         amount: 100.0,
-        date: new Date(2022, 15, 28),
+        date: new Date(2022, 1, 14),
     },
     {
         title: 'House Insurance',
         amount: 100.0,
-        date: new Date(2022, 15, 28),
+        date: new Date(2022, 2, 15),
     },
     {
         title: 'Person Insurance',
         amount: 100.0,
-        date: new Date(2022, 15, 28),
+        date: new Date(2022, 3, 16),
     },
     {
         title: 'Money Insurance',
         amount: 100.0,
-        date: new Date(2022, 15, 28),
+        date: new Date(2022, 4, 17),
     },
     {
         title: 'lala Insurance',
         amount: 100.0,
-        date: new Date(2022, 15, 28),
+        date: new Date(2022, 5, 18),
     },
 ];
 
